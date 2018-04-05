84ed2102465a9c0815a1f87787c932536888c15c
Mojmir Fendek <mfendek@silverstripe.com>
2018-04-06 11:10:32 +1200

    Many many through syntax support added.

--- src/DataFormatter/XMLDataFormatter.php
+++ src/DataFormatter/XMLDataFormatter.php
@@ -9,6 +9,7 @@ use SilverStripe\ORM\DataObject;
 use SilverStripe\ORM\DataObjectInterface;
 use SilverStripe\Control\Director;
 use SilverStripe\ORM\SS_List;
+use SilverStripe\RestfulServer\RestfulServer;
 
 /**
  * Formats a DataObject's member fields into an XML string
@@ -140,6 +141,8 @@ class XMLDataFormatter extends DataFormatter
             }
 
             foreach ($obj->manyMany() as $relName => $relClass) {
+                $relClass = RestfulServer::parseRelationClass($relClass);
+
                 //remove dot notation from relation names
                 $parts = explode('.', $relClass);
                 $relClass = array_shift($parts);
